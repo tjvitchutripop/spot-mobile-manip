bcabeb0ba230f801a4a4fc9fc44faeb920a85f08
TJ Vitchutripop <tjvitchutripop@TJs-MacBook-Pro-2.local>
2024-09-26 15:52:59 -0400

    Initial commit. Contains mobile manipulation setup with Spot

---
+++ tutorial.py
@@ -0,0 +1,39 @@
+import mujoco
+import mujoco.viewer
+import matplotlib.pyplot as plt
+# Other imports and helper functions
+import time
+import itertools
+import numpy as np
+
+# from robot_descriptions import spot_mj_description
+
+
+m = mujoco.MjModel.from_xml_path("scene.xml")
+# m = mujoco.MjModel.from_xml_path(spot_mj_description.MJCF_PATH)
+d = mujoco.MjData(m)
+
+with mujoco.viewer.launch_passive(m, d) as viewer:
+  # Close the viewer automatically after 30 wall-seconds.
+  # start = time.time()
+  # while viewer.is_running() and time.time() - start < 30:
+  while viewer.is_running() and True:
+
+    # step_start = time.time()
+
+    # mj_step can be replaced with code that also evaluates
+    # a policy and applies a control signal before stepping the physics.
+    mujoco.mj_step(m, d)
+
+    # Example modification of a viewer option: toggle contact points every two seconds.
+    with viewer.lock():
+      viewer.opt.flags[mujoco.mjtVisFlag.mjVIS_CONTACTPOINT] = int(d.time % 2)
+
+    # Pick up changes to the physics state, apply perturbations, update options from GUI.
+    viewer.sync()
+
+    # Rudimentary time keeping, will drift relative to wall clock.
+    # time_until_next_step = m.opt.timestep - (time.time() - step_start)
+    # if time_until_next_step > 0:
+    #   time.sleep(time_until_next_step)
+  
